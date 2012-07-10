79ec31784a1e23aa043d243255cc8603930e58a6
Sebastien Barthelemy <sbarthelemy@aldebaran-robotics.com>
2012-07-10 11:28:58 +0200

    update header template to match lttng-gen-tp

--- qiprobes/tp_probes.in.h
+++ qiprobes/tp_probes.in.h
@@ -2,22 +2,20 @@
 #undef TRACEPOINT_PROVIDER
 #define TRACEPOINT_PROVIDER @_provider@
 
-#if !defined(_TRACEPOINT_@_tp_h_reinclusion_protection@) || defined(TRACEPOINT_HEADER_MULTI_READ)
-#define _TRACEPOINT_@_tp_h_reinclusion_protection@
+#undef TRACEPOINT_INCLUDE_FILE
+#define TRACEPOINT_INCLUDE_FILE @_tp_h@
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#if !defined(@_tp_h_reinclusion_protection@) || defined(TRACEPOINT_HEADER_MULTI_READ)
+#define @_tp_h_reinclusion_protection@
 
 #include <lttng/tracepoint.h>
 
 @_tp_def_contents@
-
-#endif /* _TRACEPOINT_@_tp_h_reinclusion_protection@ */
-
-#undef TRACEPOINT_INCLUDE_FILE
-#define TRACEPOINT_INCLUDE_FILE @_tp_h@
+#endif /* @_tp_h_reinclusion_protection@ */
 
 /* This part must be outside ifdef protection */
 #include <lttng/tracepoint-event.h>
