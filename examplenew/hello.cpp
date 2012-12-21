3c94d5dae63377c0f94874346e59080d261e5716
agarcia <agarcia@aldebaran-robotics.com>
2012-12-21 12:13:49 +0100

    fix uninitialized variables
    Change-Id: Ib6b1390e1ef5b1ba957405a9aff0712f36a75c10
    Reviewed-on: http://gerrit.aldebaran.lan:8080/9370
    Reviewed-by: sbarthelemy <sbarthelemy@aldebaran-robotics.com>
    Tested-by: agarcia <agarcia@aldebaran-robotics.com>

--- examplenew/hello.cpp
+++ examplenew/hello.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, const char* argv[]) {
     tracepoint(qi_probes_examplenew_hello, start);
-    for (int i; i<10; ++i)
+    for (int i=0; i<10; ++i)
     {
         say(i);
     }
